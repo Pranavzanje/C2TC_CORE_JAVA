1f647f1cfa3712e6e2690eb2d380c478de84267d
YASH <YASH@Yash07>
2022-08-21 17:51:05 +0530

    Area of Rectangle

---
+++ BATCH_M24/src/com/cg/core/RectangleArea.java
@@ -0,0 +1,23 @@
+package com.cg.core;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the length=");
+		double length = sc.nextDouble();
+		
+		System.out.println("Enter the width=");
+		double width = sc.nextDouble();
+
+		double area= length*width;
+		
+		System.out.println("Area of Rectangle=" +area);
+		
+	}
+
+}
