ec0f079403cb172a53e122f650d8aaca8b4d4093
YASH <YASH@Yash07>
2022-08-21 20:21:35 +0530

    Find Factorial of a Number (using loop)

---
+++ BATCH_M24/src/com/cg/core/FactorialUsingLoop.java
@@ -0,0 +1,20 @@
+package com.cg.core;
+
+import java.util.Scanner;
+
+public class FactorialUsingLoop {
+
+	public static void main(String[] args) {
+		 int i,fact=1;  
+		 System.out.println("Enter any number");
+			Scanner sc=new Scanner(System.in);
+
+			int number=sc.nextInt();   
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);   
+
+	}
+
+}
