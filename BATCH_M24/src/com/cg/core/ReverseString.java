0f37b20ad35d14dbe69b0f0d186d7779dc878377
YASH <YASH@Yash07>
2022-08-21 21:23:12 +0530

    Reverse of String

--- BATCH_M24/src/com/cg/core/ReverseString.java
+++ BATCH_M24/src/com/cg/core/ReverseString.java
@@ -1,21 +1,18 @@
 package com.cg.core;
 
+import java.util.Scanner;
+
 public class ReverseString {
 	
 		public static void main(String[] args) {  
-			
-			 String string = "Dream big";    
-		        //Stores the reverse of given string    
-		        String reversedStr = "";    
-		            
-		        //Iterate through the string from last and add each character to variable reversedStr    
-		        for(int i = string.length()-1; i >= 0; i--){    
-		            reversedStr = reversedStr + string.charAt(i);    
-		        }    
-		            
-		        System.out.println("Original string: " + string);    
-		        //Displays the reverse of given string    
-		        System.out.println("Reverse of given string: " + reversedStr);    
+			  String original, reverse = ""; 
+		      Scanner in = new Scanner(System.in);   
+		      System.out.println("Enter a string");  
+		      original = in.nextLine();   
+		      int length = original.length();   
+		      for ( int i = length - 1; i >= 0; i-- )  
+		         reverse = reverse + original.charAt(i);  
+		      System.out.println("Reverse string=" +reverse);
 		    }    
 		}    
 		
