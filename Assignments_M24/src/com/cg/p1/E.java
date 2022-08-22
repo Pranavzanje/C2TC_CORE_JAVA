8250f75b08e22bcc7ecc44e4b052d2a6f3a592dc
YASH <YASH@Yash07>
2022-08-22 23:29:46 +0530

    private access modifier

---
+++ Assignments_M24/src/com/cg/p1/E.java
@@ -0,0 +1,8 @@
+package com.cg.p1;
+
+public class E {
+	void display()
+	 {
+	 System.out.println("TNS Sessions");
+}
+}
---
+++ Assignments_M24/src/com/cg/p1/F.java
@@ -0,0 +1,12 @@
+package com.cg.p1;
+
+public class F {
+
+	public static void main(String[] args) {
+		E obj = new E();
+		 //trying to access private method of another class
+		 obj.display();
+
+	}
+
+}
