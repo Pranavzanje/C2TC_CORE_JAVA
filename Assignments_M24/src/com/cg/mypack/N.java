28c9b124d3bd16043d62ce59fa1a4023e0232630
YASH <YASH@Yash07>
2022-08-22 23:12:02 +0530

    Assignment 1

---
+++ Assignments_M24/src/com/cg/mypack/N.java
@@ -0,0 +1,16 @@
+package com.cg.mypack;
+import com.cg.pack.*;
+import com.cg.subpack.*;
+public class N {
+
+	public static void main(String[] args) {
+		X obj = new X(); 
+		 Y obj1 = new Y();
+		 M obj2 = new M();
+		 obj.msg(); 
+		 obj1.msg(); 
+		 obj2.msg();
+
+	}
+
+}
---
+++ Assignments_M24/src/com/cg/subpack/M.java
@@ -0,0 +1,8 @@
+package com.cg.subpack;
+
+public class M {
+	public void msg()
+	{
+	System.out.println("Hello from class M-SubPackage");
+	}
+	}
