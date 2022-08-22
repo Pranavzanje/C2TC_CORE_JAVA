921f81d219270e4067ba13e7885de99c2fcf6308
YASH <YASH@Yash07>
2022-08-22 23:35:07 +0530

    Assignment1

---
+++ Assignments_M24/src/com/cg/p1/Myclass1.java
@@ -0,0 +1,10 @@
+package com.cg.p1;
+
+public class Myclass1 {
+	public void display()
+	 {
+	 System.out.println("Hello World!");
+
+	}
+
+}
---
+++ Assignments_M24/src/com/cg/p2/Myclass2.java
@@ -0,0 +1,11 @@
+package com.cg.p2;
+import com.cg.p1.*;
+public class Myclass2 {
+
+	public static void main(String[] args) {
+		//accessing class MyClass1 from package p1
+		 Myclass1 obj = new Myclass1();
+		 obj.display();
+	}
+
+}
