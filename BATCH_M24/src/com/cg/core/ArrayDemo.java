31ba9fabf110fe287651c7afd1872bb01f03e7dd
YASH <YASH@Yash07>
2022-08-24 13:42:14 +0530

    CHAPTER on Array (1D,2D,3D Arrays),(How to create array)

--- BATCH_M24/src/com/cg/core/ArrayDemo.java
+++ BATCH_M24/src/com/cg/core/ArrayDemo.java
@@ -8,11 +8,11 @@ public class ArrayDemo {
 
 		
 		// Array 2D
-		//int [][] x=new int[2][3];
-		//System.out.println(x); // address
-		//System.out.println(x[0]);// address
-		//System.out.println(x[0][0]);// 0
-		//System.out.println(x[0][1]);//0
+		int [][] x=new int[2][3];
+		System.out.println(x); // address
+		System.out.println(x[0]);// address
+		System.out.println(x[0][0]);// 0
+		System.out.println(x[0][1]);//0
 		
 		
 		// Array 3D
