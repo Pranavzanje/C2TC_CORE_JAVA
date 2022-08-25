a099b1b59e3bca0322b4dd54be49d52c5bdb0426
YASH <YASH@Yash07>
2022-08-25 18:11:57 +0530

    BinarySearch() example

---
+++ Exploring_Inbuilt_ArrayClass_Methods/src/com/cg/array/BinarySearchMethodExample.java
@@ -0,0 +1,19 @@
+package com.cg.array;
+
+import java.util.Arrays;
+
+public class BinarySearchMethodExample {
+
+	public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int result = Arrays.binarySearch(arr,key);  
+        if (result < 0)  
+            System.out.println("Element is not found!");  
+        else  
+            System.out.println("Element is found at index: "+result);  
+
+
+	}
+
+}
