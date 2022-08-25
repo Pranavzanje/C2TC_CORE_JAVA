b2c0e7336b34368f90ad823c119c4563abbdfe9c
YASH <YASH@Yash07>
2022-08-25 18:25:47 +0530

    DeepEqual() example

---
+++ Exploring_Inbuilt_ArrayClass_Methods/src/com/cg/array/DeepEqualEx.java
@@ -0,0 +1,23 @@
+package com.cg.array;
+
+import java.util.Arrays;
+
+public class DeepEqualEx {
+
+	public static void main(String[] args) {
+		// Get the Arrays
+        int intArr[][] = { { 10, 20, 15, 22, 35 } };
+  
+        // Get the second Arrays
+        int intArr1[][] = { { 10, 15, 22 } };
+  
+        // To compare both arrays
+        System.out.println("Integer Arrays on comparison: "
+                           + Arrays.deepEquals(intArr, intArr1));
+
+
+	}
+
+}
+//Returns true if the two
+//specified arrays are deeply equal to one another.
