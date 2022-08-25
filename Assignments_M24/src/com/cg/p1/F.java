5b5f2658a856efba55b265b932486f313bb8a9f5
YASH <YASH@Yash07>
2022-08-25 18:08:27 +0530

    aslist() example

--- Assignments_M24/src/com/cg/p1/F.java
+++ Assignments_M24/src/com/cg/p1/F.java
@@ -3,10 +3,14 @@ package com.cg.p1;
 public class F {
 
 	public static void main(String[] args) {
-		E obj = new E();
-		 //trying to access private method of another class
-		 obj.display();
-
+		int [][][] x= {{{10,20,30},{40,50,60}},{{70,80},{90,100,}}};
+	
+	System.out.println(x);
+	System.out.println(x[0][1][2]);
+	System.out.println(x[1][0][1]);
+	System.out.println(x[1][2][0]);
+	System.out.println(x[1][1][1]);
+	System.out.println(x);
 	}
 
 }
---
+++ Exploring_Inbuilt_ArrayClass_Methods/src/com/cg/array/AslistMethodExample.java
@@ -0,0 +1,14 @@
+package com.cg.array;
+import java.util.Arrays;
+public class AslistMethodExample {
+
+	public static void main(String[] args) {
+		
+				int intArr[] = { 10, 20, 15, 22, 35 };
+				System.out.println("Integer Array as List: "
+		                + Arrays.asList(intArr));
+			}
+
+		}
+
+      //Returns a fixed-size list backed by the specified Arrays
