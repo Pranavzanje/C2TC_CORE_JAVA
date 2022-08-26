013f6d16c51b9535d40d529bd026ddd58678a514
YASH <YASH@Yash07>
2022-08-26 14:59:51 +0530

    String Demo

--- BATCH_M24/src/com/cg/core/StringDemo.java
+++ BATCH_M24/src/com/cg/core/StringDemo.java
@@ -1,1 +1,30 @@
+package com.cg.core;
+ public class StringDemo {
+	 public static void main(String[] args) {
+	 String s=new String("pranav");
 
+	 System.out.println("Zanje");
+//	 immutable
+	 System.out.println(s);
+	 
+//	 StringBuffer s1=new StringBuffer("Pranav");
+//	 a1.append("Zanje");
+//	 mutable
+//   System.out.println(s1);
+	 
+	 System.out.println(s.length());
+	 
+	 
+	 String s1="HELLO FRIEND";
+	 String lower=s1.toLowerCase();
+	 System.out.println(lower);
+	 
+	 String upper=s1.toLowerCase();
+	 System.out.println(upper);
+	 
+	 
+	 System.out.println(s.substring(2)); // begin index --->start index will be included.
+	 System.out.println(s.substring(2,4));//start index --->include.....end index --->exclude.
+	 
+	 }
+ }
