503cc70de9342fb3b4d3c27c0d723cfdf3992cd5
YASH <YASH@Yash07>
2022-08-28 16:48:15 +0530

    Java String endsWith()

--- Exploring_Inbuilt_StringClass_Methods/src/com/cg/stringex/EndsWithExample.java
+++ Exploring_Inbuilt_StringClass_Methods/src/com/cg/stringex/EndsWithExample.java
@@ -3,9 +3,9 @@ package com.cg.stringex;
 public class EndsWithExample {
 
 	public static void main(String[] args) {
-		String s1="java by javatpoint";  
-		System.out.println(s1.endsWith("t"));  
-		System.out.println(s1.endsWith("point")); 
+		String s1="Pranav Zanje";  
+		System.out.println(s1.endsWith("e"));  
+		System.out.println(s1.endsWith("Zanje")); 
 	}
 
 }
