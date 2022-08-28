2324b8862939d2974bb7712bb7aba1baeaf5a0e4
YASH <YASH@Yash07>
2022-08-28 16:31:59 +0530

    Java String charAt()

---
+++ Exploring_Inbuilt_StringClass_Methods/src/com/cg/stringex/CharAtExample.java
@@ -0,0 +1,15 @@
+package com.cg.stringex;
+
+public class CharAtExample {
+
+	public static void main(String[] args) {
+		 
+			String name="javatpoint";  
+			char ch=name.charAt(4);//returns the char value at the 4th index  
+			System.out.println(ch);  
+			
+	}
+
+}
+//The Java String class charAt() method returns a char value at 
+//the given index number.
