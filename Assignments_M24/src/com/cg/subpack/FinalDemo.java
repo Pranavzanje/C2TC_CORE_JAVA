62802853509bdb666d4969371d2a95e3e0ec4328
YASH <YASH@Yash07>
2022-08-29 18:23:02 +0530

    final acceess modifiers

---
+++ Assignments_M24/src/com/cg/subpack/FinalDemo.java
@@ -0,0 +1,32 @@
+package com.cg.subpack;
+
+public class FinalDemo {
+
+	public static void main(String[] args) {
+//		int b=25;
+//		later i can change
+	
+		
+//		final int a=25; //cant be change
+	
+	
+	 class Parent{
+		void property()
+		{System.out.println("house wealth plots jwellery car");
+		}
+			final void marry()
+			{
+				System.out.println("Srilatha");
+			}
+		
+	
+	class Child extends Parent 
+	{
+//		void marry()
+	}
+	{System.out.println("katrina");
+	}
+	}
+	
+	}
+}
