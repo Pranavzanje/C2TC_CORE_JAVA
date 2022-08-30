fe93125d8c580ff1c2445e3ccfa59a0835300ce6
YASH <YASH@Yash07>
2022-08-30 12:44:07 +0530

    Final keyword in java

--- Assignments_M24/src/com/cg/subpack/FinalDemo.java
+++ Assignments_M24/src/com/cg/subpack/FinalDemo.java
@@ -30,3 +30,9 @@ public class FinalDemo {
 	
 	}
 }
+
+//The final keyword in java is used to restrict the user. The java final keyword can be used in many context. Final can be:
+//
+//1.variable
+//2.method
+//3.class
