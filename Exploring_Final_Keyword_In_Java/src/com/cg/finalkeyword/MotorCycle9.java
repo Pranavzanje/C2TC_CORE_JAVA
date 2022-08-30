1149092cd27f1c2d1fbe19df7ebc1661bcc0ed52
YASH <YASH@Yash07>
2022-08-30 12:43:40 +0530

    Java final variable

---
+++ Exploring_Final_Keyword_In_Java/src/com/cg/finalkeyword/MotorCycle9.java
@@ -0,0 +1,18 @@
+package com.cg.finalkeyword;
+
+public class MotorCycle9 {
+	 
+		 final int speedlimit=90;//final variable  
+		 void run(){  
+		  speedlimit=400;  
+		 }  
+		 public static void main(String args[])
+		 {  
+		 MotorCycle9 obj=new  MotorCycle9();  
+		 obj.run();
+
+	}
+
+}
+//If you make any variable as final, you cannot change the value of final 
+//variable(It will be constant).
