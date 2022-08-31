167c0eab1613a6aea05657f3160591cb6ebe9a2b
YASH <YASH@Yash07>
2022-08-31 12:38:42 +0530

    .

---
+++ Assignments_M24/src/com/cg/mypack/StringDemo.java
@@ -0,0 +1,26 @@
+package com.cg.mypack;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+//		String s1=new String("pranav");
+//		String s2=new String("pranav");
+//		String s3="pranav";
+//		String s4="pranav";
+		
+		
+		String s1= new String("PRANAV");
+		String s2= new String("PRANAV");
+		String s3= new String("PRA");
+		
+//		System.out.println(s1.equals(s2)); //for password	
+//		System.out.println(s1.equalsIgnoreCase(s2));//for username
+
+		
+//		System.out.println(s1==s2);
+	
+	System.out.println(s1.compareTo(s2));
+	System.out.println(s1.compareTo(s3));
+	}
+
+}
