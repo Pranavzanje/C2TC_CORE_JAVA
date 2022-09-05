a97d5c77aefb953aaff8f4f5c4bb8460d6eb852b
YASH <YASH@Yash07>
2022-09-05 13:52:44 +0530

    homework

---
+++ Assignments_M24/src/com/cg/subpack/StaticDemo.java
@@ -0,0 +1,20 @@
+package com.cg.subpack;
+
+public class StaticDemo {
+
+	 static int add(int a,int b, int c, int d ) {
+		
+		 return a+b+c+d;
+	 
+		 
+		 
+		
+}
+		 public static void main(String []args) {
+			 System.out.println(StaticDemo.add (10,20,0,0));
+			 System.out.println(StaticDemo.add (10,20,30,0));
+		System.out.println(StaticDemo.add (10,20,30,40));
+			}
+
+}
+
