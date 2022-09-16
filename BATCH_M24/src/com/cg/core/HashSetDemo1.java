7613f417874bf52da28ed088929deb28ff9f5dbf
YASH <YASH@Yash07>
2022-09-16 14:10:55 +0530

    .

---
+++ BATCH_M24/src/com/cg/core/HashSetDemo1.java
@@ -0,0 +1,29 @@
+package com.cg.core;
+import java.util.*;
+public class HashSetDemo1 {
+//
+	public static void main(String[] args) {
+//		TreeSet t=new TreeSet();
+//		t.add("1");
+//		t.add("2");
+//		t.add("3");
+//		t.add("4");
+//		t.add("5");
+//		t.add("6");
+//		t.add("7");
+//		
+//		System.out.println(t);
+//		
+		
+		
+		TreeSet<String> t=new TreeSet<String>();
+		t.add("Pranav");
+		t.add("Thor");
+		t.add("Shubham");
+		t.add("Vishwajeet");
+		t.add("Bunty");
+		System.out.println(t);
+//		
+	}
+
+}
