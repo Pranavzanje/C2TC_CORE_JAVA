7bac37ebe74a5cc49df71dc599670c55351ea09f
YASH <YASH@Yash07>
2022-09-19 13:45:20 +0530

    Exploring Set interface

---
+++ BATCH_M24/src/com/cg/core/SetDemo.java
@@ -0,0 +1,15 @@
+package com.cg.core;
+import java.util.*;
+public class SetDemo {
+
+	public static void main(String[] args) {
+		   Set<String> data = new LinkedHashSet<String>();   
+		    
+	        data.add("Pranav");   
+	        data.add("Set");   
+	        data.add("Example");   
+	        data.add("Set");   
+	    
+	        System.out.println(data);   
+	    }   
+	}   
