69ccda299e970bbf005355bc8740c1c3b44bc3b7
YASH <YASH@Yash07>
2022-09-21 17:53:27 +0530

    testing

--- Assignments_M24/src/com/cg/p2/Myclass2.java
+++ Assignments_M24/src/com/cg/p2/Myclass2.java
@@ -1,11 +1,40 @@
 package com.cg.p2;
-import com.cg.p1.Myclass1;
-public class Myclass2 {
 
-	public static void main(String[] args) {
-		//accessing class MyClass1 from package com.cg.p1
-		 Myclass1 obj = new Myclass1();
-		 obj.display();
-	}
+import java.util.*;
 
-}
+
+ public class Myclass2{
+	 public static void main(String[] args) {   
+		 
+		
+		 
+		 Double num;
+	        Scanner sc= new Scanner(System.in);
+	 
+	        System.out.print("Enter a number: ");
+	        num=sc.nextDouble();
+	         
+	        
+	        Double square = num*num;
+	        System.out.println("Square of "+ num + " is: "+ square);
+	        
+	        
+	        Scanner sc1= new Scanner(System.in); //System.in is a standard input stream  
+	        System.out.print("Enter a string: ");  
+	        String str= sc1.nextLine();              //reads string   
+	        
+	        System.out.print("You have entered: "+str);  
+	     ;    
+	        int count = 0;   
+	        
+	            
+	        //Counts each character except space    
+	        for(int i = 0; i < str.length(); i++) {    
+	            if(str.charAt(i) != ' ')    
+	                count++;    
+	        }    
+	            
+	        //Displays the total number of characters present in the given string    
+	        System.out.println("Total number of characters in a string: " + count);    
+	    }    
+	}   
